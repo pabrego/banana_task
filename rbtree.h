e151010853dc16d4e4df397119ed704d8f7246ab
fabarza <41338394+fabarza@users.noreply.github.com>
2018-07-19 22:19:31 -0400

    Con funcion de comparacion

--- rbtree.h
+++ rbtree.h
@@ -1,9 +1,10 @@
 #ifndef RBTREE_H
 #define RBTREE_H
+#include "Tareas.h"
 
 typedef struct rbtree RBTree;
 
-RBTree* create_RBTree();
+RBTree* create_RBTree(lower_than);
 
 // Inserta un nuevo "data" al arbol con su respectiva "key".
 void RB_insert(RBTree* T, int key, void* data);
