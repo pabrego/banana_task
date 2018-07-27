ce3d94c55f402dc624a51dc1e0907c82f7503202
fabarza <41338394+fabarza@users.noreply.github.com>
2018-07-26 22:32:06 -0400

    Header sorpresa ÚwÙ

--- menu.h
+++ menu.h
@@ -16,26 +16,21 @@ void ajustar_pantalla ()
     system("mode con: cols=100 lines=30");
 
 
-    //Get the window console handle(isn't the right code but works for these sample
 	HWND ConsoleWindow;
 	ConsoleWindow=GetForegroundWindow();
 
-	//Getting the desktop hadle and rectangule
 	HWND   hwndScreen;
 	RECT   rectScreen;
 	hwndScreen=GetDesktopWindow ();
 	GetWindowRect(hwndScreen,&rectScreen);
 
-	//Set windows size(see the width problem)
 	SetWindowPos (ConsoleWindow,NULL,0,0,1000,500, SWP_SHOWWINDOW);
 
-	// Get the current width and height of the console
 	RECT rConsole;
 	GetWindowRect (ConsoleWindow,&rConsole);
 	int Width = rConsole.left = rConsole.right;
 	int Height = rConsole.bottom - rConsole.top;
 
-	//caculate the window console to center of the screen
 	int ConsolePosX;
 	int ConsolePosY;
 	ConsolePosX = ((rectScreen.right-rectScreen.left)/2-Width/2 );
